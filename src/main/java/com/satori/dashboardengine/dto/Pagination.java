c3328fbd34049973db6414fba1af8b9d7de18888
JGG <137589060+JGG07@users.noreply.github.com>
2024-12-17 09:39:32 -0600

    Pequeños cambios

--- src/main/java/com/satori/dashboardengine/dto/Pagination.java
+++ src/main/java/com/satori/dashboardengine/dto/Pagination.java
@@ -9,10 +9,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Pagination {
+
+    @JsonProperty("start")
     private int start;
+    @JsonProperty("limit")
     private int limit;
     @JsonProperty("more_items_in_collection")
     private boolean moreItems;
     @JsonProperty("next_start")
     private int nextStart;
+
 }
